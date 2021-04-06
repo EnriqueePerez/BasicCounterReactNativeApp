de7a5e3994c3a79c9501998543b9ba6d7551ca25
Enrique Pérez <enrike_084@hotmail.com>
2021-04-06 13:47:17 -0500

    Change CounterScreen styles to StyleSheet object

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -1,20 +1,34 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, {useState} from 'react';
-import {Text, TouchableOpacity, View} from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export const CounterScreen = () => {
   const [counter, setCounter] = useState(10);
 
   return (
-    <View style={{flex: 1, justifyContent: 'center'}}>
-      <Text style={{textAlign: 'center', fontSize: 40, top: -15}}>
-        Contador: {counter}
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Contador: {counter}</Text>
       <TouchableOpacity onPress={() => setCounter(counter + 1)}>
-        <View style={{backgroundColor: 'red', borderRadius: 100}}>
+        <View style={styles.incrementButton}>
           <Text>Click me!!</Text>
         </View>
       </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 40,
+    top: -15,
+  },
+  incrementButton: {
+    backgroundColor: 'red',
+    borderRadius: 100,
+  },
+});
