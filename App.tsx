96bd51b0a11e736e9fb8534522e0bcfe691a1144
Enrique Pérez <enrike_084@hotmail.com>
2021-04-06 13:17:49 -0500

    Added basic CounterScreen with button to increment the counter

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import {HelloWorldScreen} from './src/screens/HelloWorldScreen';
+import {CounterScreen} from './src/screens/CounterScreen';
 
 const App = () => {
-  return <HelloWorldScreen />;
+  // return <HelloWorldScreen />;
+  return <CounterScreen />;
 };
 
 export default App;
