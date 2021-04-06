daea8801e79db952f77188e30ff6d5a504abcabb
Enrique Pérez <enrike_084@hotmail.com>
2021-04-06 13:06:02 -0500

    Creating basic file structure and first screen

---
+++ src/screens/HelloWorldScreen.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export const HelloWorldScreen = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center'}}>
+      <Text
+        style={{
+          fontSize: 45,
+          textAlign: 'center',
+        }}>
+        Hola Mundo!
+      </Text>
+    </View>
+  );
+};
