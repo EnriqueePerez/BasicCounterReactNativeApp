b2f55fc77b4dec731a761c06eb0b933fff37188c
Enrique Pérez <enrike_084@hotmail.com>
2021-04-06 14:23:16 -0500

    Created button to decrement counter on CounterScreen

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -8,9 +8,18 @@ export const CounterScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Contador: {counter}</Text>
-      <TouchableOpacity onPress={() => setCounter(counter + 1)}>
-        <View style={styles.incrementButton}>
-          <Text>Click me!!</Text>
+      <TouchableOpacity
+        style={styles.fabLocationBR}
+        onPress={() => setCounter(counter + 1)}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>+1</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.fabLocationBL}
+        onPress={() => setCounter(counter - 1)}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>-1</Text>
         </View>
       </TouchableOpacity>
     </View>
@@ -27,8 +36,27 @@ const styles = StyleSheet.create({
     fontSize: 40,
     top: -15,
   },
-  incrementButton: {
-    backgroundColor: 'red',
+  fabLocationBR: {
+    position: 'absolute',
+    bottom: 25,
+    right: 25,
+  },
+  fabLocationBL: {
+    position: 'absolute',
+    bottom: 25,
+    left: 25,
+  },
+  fab: {
+    backgroundColor: '#5856D6',
+    width: 60,
+    height: 60,
     borderRadius: 100,
+    justifyContent: 'center',
+  },
+  fabText: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold',
+    alignSelf: 'center',
   },
 });
