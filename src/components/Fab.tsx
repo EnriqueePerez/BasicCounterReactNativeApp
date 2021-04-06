f9fd3188a41b18ea2afc065f55e5012cb3cd9987
Enrique Pérez <enrike_084@hotmail.com>
2021-04-06 15:36:51 -0500

    Adding shadow to the fab and correcting styles to show the touchable feedback in each OS

--- src/components/Fab.tsx
+++ src/components/Fab.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
+import {
+  TouchableOpacity,
+  View,
+  Text,
+  StyleSheet,
+  TouchableNativeFeedback,
+  Platform,
+} from 'react-native';
 
 //creating an inteface to props to auto document the new component
 //the functions should be at the end of the interface
@@ -9,25 +16,56 @@ interface Props {
   onPress: () => void;
 }
 
-export const Fab = ({ title, onPress }: Props) => {
-  return (
-    <TouchableOpacity style={styles.fabLocationBR} onPress={onPress}>
-      <View style={styles.fab}>
-        <Text style={styles.fabText}>{title}</Text>
+export const Fab = ({ title, onPress, position = 'bottomRight' }: Props) => {
+  const android = () => {
+    return (
+      <View
+        //adding a array of styles to change position of the touchable according to the prop position
+        style={[
+          styles.fabLocation,
+          position === 'bottomLeft' ? styles.left : styles.right,
+        ]}>
+        <TouchableNativeFeedback
+          onPress={onPress}
+          //Ripple method only works on Android not on IOS
+          background={TouchableNativeFeedback.Ripple('black', false, 30)}>
+          <View style={styles.fab}>
+            <Text style={styles.fabText}>{title}</Text>
+          </View>
+        </TouchableNativeFeedback>
       </View>
-    </TouchableOpacity>
-  );
+    );
+  };
+
+  const ios = () => {
+    return (
+      <View
+        //adding a array of styles to change position of the touchable according to the prop position
+        style={[
+          styles.fabLocation,
+          position === 'bottomLeft' ? styles.left : styles.right,
+        ]}>
+        <TouchableOpacity activeOpacity={0.8} onPress={onPress}>
+          <View style={styles.fab}>
+            <Text style={styles.fabText}>{title}</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
+  return Platform.OS === 'ios' ? ios() : android();
 };
 
 const styles = StyleSheet.create({
-  fabLocationBR: {
+  fabLocation: {
     position: 'absolute',
     bottom: 25,
+  },
+  right: {
     right: 25,
   },
-  fabLocationBL: {
-    position: 'absolute',
-    bottom: 25,
+  left: {
     left: 25,
   },
   fab: {
@@ -36,6 +74,16 @@ const styles = StyleSheet.create({
     height: 60,
     borderRadius: 100,
     justifyContent: 'center',
+
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
   },
   fabText: {
     color: 'white',
