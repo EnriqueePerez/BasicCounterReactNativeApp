747674375e17db50261830669faa0ea0d91ca96a
Enrique Pérez <enrike_084@hotmail.com>
2021-04-07 12:11:48 -0500

    Adding new FlexScreen to show examples with flex

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 import { PositionScreen } from './src/screens/PositionScreen';
+import { FlexScreen } from './src/screens/FlexScreen';
 
 const App = () => {
   return (
@@ -9,7 +10,8 @@ const App = () => {
      <CounterScreen /> */}
       {/* <BoxObjectModelScreen /> */}
       {/* <DimensionsScreen /> */}
-      <PositionScreen />
+      {/* <PositionScreen /> */}
+      <FlexScreen />
     </SafeAreaView>
   );
 };
