16f9eac16b4a5806e4734fa524f9f77c72c81d31
Enrique Pérez <enrike_084@hotmail.com>
2021-04-07 10:09:24 -0500

    Basic examples wiht position added to PositionScreen

--- src/screens/PositionScreen.tsx
+++ src/screens/PositionScreen.tsx
@@ -4,6 +4,7 @@ import { View, StyleSheet } from 'react-native';
 export const PositionScreen = () => {
   return (
     <View style={styles.container}>
+      <View style={styles.cajaVerde} />
       <View style={styles.cajaMorada} />
       <View style={styles.cajaNaranja} />
     </View>
@@ -11,10 +12,11 @@ export const PositionScreen = () => {
 };
 
 const styles = StyleSheet.create({
+  //All items are by default position : relative. All items are relative to their father
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
     backgroundColor: '#28C4D9',
   },
   cajaMorada: {
@@ -23,6 +25,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#5856D6',
     borderWidth: 10,
     borderColor: 'white',
+    position: 'absolute',
+    top: 0,
+    right: 0,
   },
   cajaNaranja: {
     width: 100,
@@ -30,5 +35,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#F0A23B',
     borderWidth: 10,
     borderColor: 'white',
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+  },
+  cajaVerde: {
+    backgroundColor: 'green',
+    borderWidth: 10,
+    borderColor: 'white',
+    // position: 'absolute',
+    // bottom: 0,
+    // left: 0,
+    // top: 0,
+    // right: 0,
+    //this object is equivalent to write the same thing above
+    ...StyleSheet.absoluteFillObject,
   },
 });
